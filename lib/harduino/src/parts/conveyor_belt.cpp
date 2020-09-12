f22b9d170f0edaaa2f422645252ffaad2152ad78
Ocead <johannes.milczewski@ocead.de>
2020-09-12 21:19:24 +0200

    Implemented motor and conveyor belt

--- lib/harduino/src/parts/conveyor_belt.cpp
+++ lib/harduino/src/parts/conveyor_belt.cpp
@@ -69,11 +69,47 @@ part duino::parts::conveyor_belt(part_h offset) {
     };
 
     pt.delegates.cycle = [](cell & cl) {
+        auto & gcl = cl.as_grid_cell();
+        uint_t distance = std::numeric_limits<uint_t>::max();
 
+        for (auto dir : direction::cardinal) {
+            double_t speed = 0.;
+            auto & ncl = gcl[dir];
+
+            if (ncl.has(of::MOTOR_SPEED)) {
+                speed = double_t(ncl[MOTOR_SPEED]);
+                distance = 0u;
+            } else if (ncl.has(of::MOVING_TO)) {
+                double_t dir_fac;
+                if (ncl[of::MOVING_TO] == value(!dir)) {
+                    dir_fac = 1;
+                } else if (ncl[of::MOVING_FROM] == value(!dir)) {
+                    dir_fac = -1;
+                } else {
+                    continue;
+                }
+
+                auto ndistance = uint_t(ncl[of::MOTOR_DISTANCE]);
+                if (ndistance + 1 < distance && ndistance != std::numeric_limits<uint_t>::max()) {
+                    speed = double_t(ncl[value::moving(!dir)]) * dir_fac;
+                    distance = ndistance + 1;
+                } else {
+                    continue;
+                }
+            } else {
+                continue;
+            }
+
+            replace(cl[value::moved(dir)], speed);
+            replace(cl[value::moving(direction_t(cl[of::MOVING_TO]))], speed);
+            replace(cl[value::moving(direction_t(cl[of::MOVING_FROM]))], -speed);
+        }
+
+        replace(cl[of::MOTOR_DISTANCE], distance);
     };
 
     pt.delegates.move = [](cell & cl) {
-        for (auto & [num, cell] : cl.as_grid_cell().cargo()) {
+        for (auto &[num, cell] : cl.as_grid_cell().cargo()) {
 
         }
     };
@@ -86,28 +122,7 @@ part duino::parts::conveyor_belt(part_h offset) {
             auto from = direction_t(cl[of::MOVING_FROM]);
             auto to = direction_t(cl[of::MOVING_TO]);
 
-            cr->translate(128., 128.);
-            switch (from) {
-                case raw_direction::UP: {
-                    cr->rotate(-M_PI_2);
-                    break;
-                }
-                case raw_direction::DOWN: {
-                    cr->rotate(M_PI_2);
-                    break;
-                }
-                case raw_direction::RIGHT: {
-                    break;
-                }
-                case raw_direction::LEFT: {
-                    cr->rotate(M_PI);
-                    break;
-                }
-                default: {
-                    break;
-                }
-            }
-            cr->translate(-128., -128.);
+            rotate_cardinal(cr, from);
 
             auto color = color_t(cl[of::COLOR]);
 
@@ -134,14 +149,14 @@ part duino::parts::conveyor_belt(part_h offset) {
                 cr->stroke();
             } else if (from == cw(to)) {
                 cr->set_source_rgb(color.r / 255., color.g / 255., color.b / 255.);
-                cr->arc(256., 0., 256. - 32.,0., 2 * M_PI);
-                cr->arc(256., 0., 32.,0., 2 * M_PI);
+                cr->arc(256., 0., 256. - 32., 0., 2 * M_PI);
+                cr->arc(256., 0., 32., 0., 2 * M_PI);
                 cr->set_line_width(16.);
                 cr->stroke();
             } else if (from == ccw(to)) {
                 cr->set_source_rgb(color.r / 255., color.g / 255., color.b / 255.);
                 cr->arc(256., 256., 256. - 32., 0., 2 * M_PI);
-                cr->arc(256., 256., 32.,0., 2 * M_PI);
+                cr->arc(256., 256., 32., 0., 2 * M_PI);
                 cr->set_line_width(16.);
                 cr->stroke();
             }
@@ -157,7 +172,8 @@ part duino::parts::conveyor_belt(part_h offset) {
                            of::MOVING_RIGHT,
                            of::MOVING_LEFT,
                            of::MOVING_FROM,
-                           of::MOVING_TO
+                           of::MOVING_TO,
+                           of::MOTOR_SPEED
                    });
 
     return pt;
--- lib/harduino/src/parts/motor.cpp
+++ lib/harduino/src/parts/motor.cpp
@@ -11,8 +11,108 @@ using namespace har::parts;
 part duino::parts::motor(part_h offset) {
     part pt{ PART[standard_ids::MOTOR + offset],
              text("har:motor"),
-             traits::COMPONENT_PART | traits::INPUT | traits::MOVING | traits::ORIENTABLE,
+             traits::COMPONENT_PART |
+             traits::INPUT |
+             traits::MOVING |
+             traits::ORIENTABLE |
+             traits::SOLID,
              text("Motor") };
+
+    add_properties_for_traits(pt, 5.);
+
+    pt.remove_entry(of::MOVING_FROM);
+    pt.remove_entry(of::MOVING_TO);
+    pt.remove_entry(of::MOTOR_DISTANCE);
+
+    pt.add_entry(entry{ of::MOTOR_SPEED,
+                        text("__MOTOR_SPEED"),
+                        text("Motor speed"),
+                        value(double_t(0.)),
+                        ui_access::VISIBLE,
+                        serialize::NO_SERIALIZE,
+                        std::array<double_t, 3>{ -1., 1., .001 }});
+
+    pt.add_entry(entry{ of::SPEED_FACTOR,
+                        text("__SPEED_FACTOR"),
+                        text("Speed factor"),
+                        value(double_t(0.)),
+                        ui_access::CHANGEABLE,
+                        serialize::NO_SERIALIZE,
+                        std::array<double_t, 3>{ 0., 1., .001 }});
+
+    pt.delegates.cycle = [](cell & cl) {
+        double_t speed = 0.;
+        double_t dir = 1.;
+
+        for (auto &[use, ncl] : cl.as_grid_cell().connected()) {
+            if (use == direction::PIN[0]) {
+                speed = double_t(cl[of::SPEED_FACTOR]) * (double_t(ncl[of::POWERING_PIN]) / double_t(cl[of::HIGH_VOLTAGE]));
+            } else if (use == direction::PIN[1]) {
+                if (double_t(ncl[of::POWERING_PIN]) > double_t(cl[of::HIGH_VOLTAGE])) {
+                    dir = -1;
+                }
+            }
+        }
+
+        speed *= dir;
+        replace(cl[of::MOTOR_SPEED], speed);
+    };
+
+    pt.delegates.draw = [](cell & cl, image_t & im) {
+        if (im.type() == typeid(ImageType)) {
+            Cairo::RefPtr<Cairo::Surface> sf = Cairo::ImageSurface::create(Cairo::FORMAT_ARGB32, 256, 256);
+            auto cr = Cairo::Context::create(sf);
+
+            rotate_cardinal(cr, direction_t(cl[of::FACING]));
+
+            cr->set_source_rgb(.75, .75, .75);
+            cr->move_to(200., 128.);
+            cr->line_to(256., 128.);
+            cr->set_line_width(24.);
+            cr->stroke();
+
+            cr->set_source_rgb(.5, .5, .5);
+            cr->move_to(16., 72.);
+            cr->line_to(32., 56.);
+            cr->line_to(192., 56.);
+            cr->line_to(208., 72.);
+            cr->line_to(208., 256. - 72.);
+            cr->line_to(192., 256. - 56.);
+            cr->line_to(32., 256. - 56.);
+            cr->line_to(16., 256. - 72.);
+            cr->fill();
+            cr->stroke();
+
+            cr->set_source_rgb(.25, .25, .25);
+            cr->move_to(64., 56.);
+            cr->line_to(64., 256. - 56.);
+            cr->move_to(160., 56.);
+            cr->line_to(160., 256. - 56.);
+            cr->move_to(64., 64.);
+            cr->line_to(160., 64.);
+            cr->move_to(64., 80.);
+            cr->line_to(160., 80.);
+            cr->move_to(64., 102.);
+            cr->line_to(160., 102.);
+            cr->move_to(64., 128.);
+            cr->line_to(160., 128.);
+            cr->move_to(64., 256. - 102.);
+            cr->line_to(160., 256. - 102.);
+            cr->move_to(64., 256. - 80.);
+            cr->line_to(160., 256. - 80.);
+            cr->move_to(64., 256. - 64.);
+            cr->line_to(160., 256. - 64.);
+            cr->set_line_width(8.);
+            cr->stroke();
+
+            im = std::make_tuple(sf, uint_t(256u));
+        }
+    };
+
+    pt.add_visuals({ of::FACING });
+
+    pt.add_connection_uses({{ direction::PIN[0], text("Enable") },
+                            { direction::PIN[1], text("Direction") }});
 
     return pt;
 }
